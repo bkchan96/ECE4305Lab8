0175b9ef05dbc381bc84d6c1e5f8a1aa8f309c93
bkchan96 <39748671+bkchan96@users.noreply.github.com>
2018-10-22 00:13:02 -0700

    Alarm 1.1.10 (almost working)
    - cleaned up alarm_counter.v

--- alarm_counter.v
+++ alarm_counter.v
@@ -4,11 +4,13 @@ module alarm_counter(reset, upsec, upmin, uphour, outsecMSB, outsecLSB, outminMS
     input reset;
     input upsec, upmin, uphour;
     output wire [3:0] outsecMSB, outsecLSB, outminMSB, outminLSB, outhourMSB, outhourLSB;
+    
+    wire incSecMSB, incMinMSB, incHourMSB;
 
     baseTenIncrementer secLSB(.inc(upsec), .reset(reset), .incNext(incSecMSB), .value(outsecLSB));
-    baseSixIncrementer secMSB(.inc(incSecMSB), .reset(reset), .incNext(incMinLSB), .value(outsecMSB));
+    baseSixIncrementer secMSB(.inc(incSecMSB), .reset(reset), .incNext(), .value(outsecMSB));
     baseTenIncrementer minLSB(.inc(upmin), .reset(reset), .incNext(incMinMSB), .value(outminLSB));
-    baseSixIncrementer minMSB(.inc(incMinMSB), .reset(reset), .incNext(incHourLSB), .value(outminMSB));
+    baseSixIncrementer minMSB(.inc(incMinMSB), .reset(reset), .incNext(), .value(outminMSB));
     baseFourIncrementer hourLSB(.inc(uphour), .reset(reset), .incNext(incHourMSB), .hourMSBvalue(outhourMSB), .value(outhourLSB));
     baseThreeIncrementer hourMSB(.inc(incHourMSB), .reset(reset), .incNext(), .value(outhourMSB));
     
